37633c14ce113ef30ce40de9b2f94d8d58c800a2
Alkquiver <jesse.mao@kingsway.school.nz>
2024-11-03 19:26:37 +1300

    Update Area and Perimeter.py

--- Area and Perimeter.py
+++ Area and Perimeter.py
@@ -3,7 +3,7 @@
 # Date: 2024-10-25
 # Version: 3
 
-def valid_num(question): # Testing the user's input, to see is it is a valid number that is larger than 1
+def valid_num(question): # Testing the user's input, to see is it is a valid number that is larger than 0
     while True:
         try:
             response = float(input(question)) # User's inout
@@ -24,6 +24,7 @@ while True:
 # Calculate the area / perimeter
     area = width * length
     perimeter = (width + length) * 2
-    print(f"The perimeter is {perimeter} units, and the area is {area} units squared")
+    print(f"The perimeter is {perimeter} units, and the area is {area} units squared") # Giving the user the results
     if input("Please press enter to calculate again. Press any other key to quit.\n"): # Gives the user the choice to loop the program
         break
+
